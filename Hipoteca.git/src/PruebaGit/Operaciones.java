6365fce9c766e159f005848731403b312da0ac41
Guillermo Gomez <a021011995t@s09-pc23.ausias.lan>
2020-02-07 20:05:25 +0100

    Agregadas las sumas y restas.

--- Hipoteca.git/src/PruebaGit/Operaciones.java
+++ Hipoteca.git/src/PruebaGit/Operaciones.java
@@ -7,5 +7,22 @@ public class Operaciones {
 		this.a=a;
 		this.b=b;
 	}
-
+	public int getA() {
+		return a;
+	}
+	public void setA(int a) {
+		this.a = a;
+	}
+	public int getB() {
+		return b;
+	}
+	public void setB(int b) {
+		this.b = b;
+	}
+	public int suma() {
+		return a+b;
+	}
+	public int resta() {
+		return a-b;
+	}
 }
